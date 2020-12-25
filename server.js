f2f0a359264c4a2abc9760898d2f71fccf18a566
Imperial Design <amsanchez@knox.edu>
2020-12-24 21:29:47 -0600

    change port variable

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./controllers/'));
 
 // turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
-  });
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
